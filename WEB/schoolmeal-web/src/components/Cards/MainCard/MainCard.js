f1d320149b8f63c8e0fbba586a792fd7b347dd08
gguggu <adrw1365@gmail.com>
2019-08-12 22:43:32 +0900

    [FIX] SchoolList, MainCard, MainList
    디자인 수정했어요

--- WEB/schoolmeal-web/src/components/Cards/MainCard/MainCard.js
+++ WEB/schoolmeal-web/src/components/Cards/MainCard/MainCard.js
@@ -6,17 +6,17 @@ const MainCard = ({ mealData }) => {
         <div className="MainCard">
             <p className="MainCard--mealDate">{mealData.date}</p>
             <div className="MainCard--mealBreakfast">
-                <p>아침</p>
+                <p className="MainCard--mealBreakfast--time">아침</p>
                 <p className="MainCard--mealBreakfast--contents">{mealData.breakfast}</p>
             </div>
 
             <div className="MainCard--mealLunch">
-                <p>점심</p>
+                <p className="MainCard--mealBreakfast--time">점심</p>
                 <p className="MainCard--mealLunch--contents">{mealData.lunch}</p>
             </div>
 
             <div className="MainCard--mealDinner">
-                <p>저녁</p>
+                <p className="MainCard--mealBreakfast--time">저녁</p>
                 <p className="MainCard--mealDinner--contents">{mealData.dinner}</p>
             </div>
         </div>
