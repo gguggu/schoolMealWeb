af2c38205f46aa2da8a68c9d39ae9aae406adc1f
gguggu <adrw1365@gmail.com>
2019-08-12 22:45:45 +0900

    [FIX] SchoolCard
    학교정보를 보여주는 카드의 내용을 보기 좋게 수정했습니다.

--- WEB/schoolmeal-web/src/components/Cards/SchoolCard/SchoolCard.js
+++ WEB/schoolmeal-web/src/components/Cards/SchoolCard/SchoolCard.js
@@ -2,22 +2,31 @@ import React from 'react';
 import './SchoolCard.scss';
 
 const SchoolCard = ({ schoolData, school, closeModal }) => {
-
+    
     const sendingSchoolCode = async() => {
         const schoolCode = schoolData.code;
         const schoolType = schoolData.type;
         console.log(schoolCode, schoolType);
+        await school.putNowSchoolData(schoolCode, schoolType);
         await school.getSchoolMeal(schoolCode, schoolType);
+        closeModal();
     }
 
     return (
         <button className="SchoolCard" onClick={() => {
             sendingSchoolCode();
-            closeModal();
         }}>
-            <p className="SchoolCard--contents">학교이름: {schoolData.name}</p>
-            <p className="SchoolCard--contents">학교타입: {schoolData.type}</p>
-            <p className="SchoolCard--contents">학교주소: {schoolData.address}</p>
+            <p className="SchoolCard--contents">학교이름: {schoolData.name}&nbsp;</p>
+            {
+                (() => {
+                    if(schoolData.type === 'elementary'){return <p className="SchoolCard--contents">학교유형: 초등학교&nbsp;</p>}
+                    else if(schoolData.type === 'middle'){return <p className="SchoolCard--contents">학교유형: 중학교&nbsp;</p>}
+                    else if(schoolData.type === 'high'){return <p className="SchoolCard--contents">학교유형: 고등학교&nbsp;</p>}
+                    else if(schoolData.type === 'special'){return <p className="SchoolCard--contents">학교유형: 특수학교&nbsp;</p>}
+                    else{return <p className="SchoolCard--contents">학교유형: X&nbsp;</p>}
+                })()
+            }
+            <p className="SchoolCard--contents">학교주소: {schoolData.address}&nbsp;</p>
         </button>
     );
 };
