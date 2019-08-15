41574b01484930cfd81a3eeaf57f85b93e768e19
gguggu <adrw1365@gmail.com>
2019-08-15 14:25:09 +0900

    [FIX] MainCard
    급식이 없는 날을 예외처리 했습니다.

--- WEB/schoolmeal-web/src/components/Cards/MainCard/MainCard.js
+++ WEB/schoolmeal-web/src/components/Cards/MainCard/MainCard.js
@@ -5,20 +5,33 @@ const MainCard = ({ mealData }) => {
     return (
         <div className="MainCard">
             <p className="MainCard--mealDate">{mealData.date}</p>
-            <div className="MainCard--mealBreakfast">
-                <p className="MainCard--mealBreakfast--time">아침</p>
-                <p className="MainCard--mealBreakfast--contents">{mealData.breakfast}</p>
-            </div>
+            {
+                (() => {
+                    if(!mealData.breakfast.length && !mealData.lunch.length && !mealData.dinner.length){
+                        return <p className="MainCard--noMeal">이날은 급식이 없어요!</p>
+                    }
+                    else{
+                        return (
+                            <div className="MainCard--mealContent">
+                                <div className="MainCard--mealContent--mealBreakfast">
+                                    <p className="MainCard--mealContent--mealBreakfast--time">아침</p>
+                                    <p className="MainCard--mealContent--mealBreakfast--contents">{mealData.breakfast}</p>
+                                </div>
 
-            <div className="MainCard--mealLunch">
-                <p className="MainCard--mealBreakfast--time">점심</p>
-                <p className="MainCard--mealLunch--contents">{mealData.lunch}</p>
-            </div>
+                                <div className="MainCard--mealContent--mealLunch">
+                                    <p className="MainCard--mealContent--mealBreakfast--time">점심</p>
+                                    <p className="MainCard--mealContent--mealLunch--contents">{mealData.lunch}</p>
+                                </div>
 
-            <div className="MainCard--mealDinner">
-                <p className="MainCard--mealBreakfast--time">저녁</p>
-                <p className="MainCard--mealDinner--contents">{mealData.dinner}</p>
-            </div>
+                                <div className="MainCard--mealContent--mealDinner">
+                                    <p className="MainCard--mealContent--mealBreakfast--time">저녁</p>
+                                    <p className="MainCard--mealContent--mealDinner--contents">{mealData.dinner}</p>
+                                </div>
+                            </div>
+                        )
+                    }
+                })()
+            }
         </div>
     );
 };
