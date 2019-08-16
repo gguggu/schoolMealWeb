51d50339c1b6b2f6104cd63988d687315203154d
gguggu <adrw1365@gmail.com>
2019-08-16 13:08:06 +0900

    [FIX] MainList
    오늘 날짜에 맞게 바로 급식나오게 수정하고 유저들이 보기 좋게 예외처리했습니다

--- WEB/schoolmeal-web/src/components/Lists/MainList/MainList.js
+++ WEB/schoolmeal-web/src/components/Lists/MainList/MainList.js
@@ -1,44 +1,53 @@
 import React from 'react';
 import './MainList.scss';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import moment from 'moment';
 
 const MainList = ({ mealList }) => {
-    const [card, setCard] = React.useState(1);
+    const today = parseInt(moment().format('D'));
+    const [card, setCard] = React.useState(today);
     return (
         <div className="MainList">
-            <div className="MainList--main">
-                <div className="MainList--main--leftContents">
-                    {
-                        card === 1
-                        ? (<></>)
-                        : (mealList[card-2])
-                    }
-                </div>
-                <IoIosArrowBack onClick={() => {
-                    if(card === 1){
-                        return;
-                    }
-                    setCard(card-1);
-                }}
-                className="MainList--main--arrowBack"
-                >
-                </IoIosArrowBack>
-                <div className="MainList--main--mainContents">
-                    {mealList.slice((card - 1), (card - 1) + 1)}
-                </div>
-                <IoIosArrowForward onClick={() => {
-                    if(card * 1 >= mealList.length){
-                        return;
-                    }
-                    setCard(card + 1);
-                }}
-                className="MainList--main--arrowForward"
-                >
-                </IoIosArrowForward>
-                <div className="MainList--main--rightContents">
-                    {mealList[card]}
-                </div>
-            </div>
+            {
+                mealList.length !== 0 ?
+                (
+                    <div className="MainList--main">
+                        <div className="MainList--main--leftContents">
+                            {
+                                card === 1
+                                ? (<></>)
+                                : (mealList[card-2])
+                            }
+                        </div>
+                        <IoIosArrowBack onClick={() => {
+                            if(card === 1){
+                                return;
+                            }
+                            setCard(card-1);
+                        }}
+                        className="MainList--main--arrowBack"
+                        >
+                        </IoIosArrowBack>
+                        <div className="MainList--main--mainContents">
+                            {mealList.slice((card - 1), (card - 1) + 1)}
+                        </div>
+                        <IoIosArrowForward onClick={() => {
+                            if(card * 1 >= mealList.length){
+                                return;
+                            }
+                            setCard(card + 1);
+                        }}
+                        className="MainList--main--arrowForward"
+                        >
+                        </IoIosArrowForward>
+                        <div className="MainList--main--rightContents">
+                            {mealList[card]}
+                        </div>
+                    </div>
+                ) : (
+                    <></>
+                )
+            }
         </div>
     );
 };
