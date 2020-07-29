ae17250e782491080cf31070bc7ae964c2c5f842
gguggu <adrw1364@naver.co>
2020-07-29 17:39:42 +0900

    [UPDATE] UPDATE Main
    메인 템플릿의 class 컴포넌트를 함수형 컴포넌트로 변경 후 학교 검색 이슈 수정

--- WEB/schoolmeal-web/src/components/Main/Main.js
+++ WEB/schoolmeal-web/src/components/Main/Main.js
@@ -4,46 +4,53 @@ import MainList from '../../components/Lists/MainList/MainList';
 import Modal from 'react-modal';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { GoSearch } from 'react-icons/go';
-import SchoolList from '../Lists/SchoolList/SchoolList';
 import Loader from '../Loader/Loader';
+import PropTypes from 'prop-types';
 
 const customStyles = {
   content : {
+    width: '500px',
+    height: '800px',
     top: '50%',
     left: '50%',
     right: 'auto',
     bottom: 'auto',
-    marginRight: '-50%',
-    height: '50em',
     transform: 'translate(-50%, -50%)',
-    backgroundColor: '#F25E5E',
-    borderRadius: '10px',
+    backgroundColor: '#fafafa',
+    borderRadius: '3px',
     boxShadow: '0 0 15px #424242',
     border: '0',
     outline: '0',
+    padding: '0'
   }
 };
 
-const Main = ({ inputingSchool, gettingSchool, closeModal, gettingMonthMeal, mealList, schoolList, store, modalIsOpen }) => {
+const Main = ({ inputSchool, getSchool, closeModal,
+  mealList, schoolList, modalIsOpen, clickMonth, month, isLoad }) => {
   return (
     <div className="mealList">
       <div className="mealList--head">
         <p className="mealList--head--title">급식!소식!</p>
         <div className="mealList--head--inputElement">
-            <input type="text" className="mealList--head--inputElement--searchBox" placeholder="학교 이름을 입력하세요." onChange={inputingSchool}/>
-            <GoSearch onClick={gettingSchool} size="2em" className="mealList--head--inputElement--search"/>
+            <input type="text" className="mealList--head--inputElement--searchBox"
+              placeholder="학교 이름을 입력하세요." onKeyUp={e => {
+                if(e.keyCode === 13)
+                  getSchool();
+              }} onChange={inputSchool}/>
+            <GoSearch onClick={getSchool} size="2em" className="mealList--head--inputElement--search"/>
         </div>
       </div>
       <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
           style={customStyles}
+          ariaHideApp={false}
       >
       {
-          store.meal.isLoad === true ? (
-            <SchoolList 
-              schoolList={schoolList}
-            />
+          isLoad === true ? (
+            <div className="mealList-SchoolList">
+              {schoolList}
+            </div>
           ) : (
             <Loader/>
           )
@@ -54,33 +61,11 @@ const Main = ({ inputingSchool, gettingSchool, closeModal, gettingMonthMeal, mea
           {
             mealList.length !== 0 ? (
               <>
-              <IoIosArrowBack onClick={() => {
-                  if(store.meal.howMonth === 1){
-                      return;
-                  }
-                  store.meal.howMonth -= 1;
-                  console.log(store.meal.howMonth);
-                  if(store.meal.importing===true){
-                    gettingMonthMeal();
-                  }
-              }}
-              className="mealList--body--month--arrowBack"
-              >
-              </IoIosArrowBack>
-              <p>{store.meal.howMonth}월</p>
-              <IoIosArrowForward onClick={() => {
-                  if(store.meal.howMonth * 1 >= 12){
-                      return;
-                  }
-                  store.meal.howMonth += 1;
-                  console.log(store.meal.howMonth);
-                  if(store.meal.importing===true){
-                    gettingMonthMeal();
-                  }
-              }}
-              className="mealList--body--month--arrowForward"
-              >
-              </IoIosArrowForward>
+                <IoIosArrowBack onClick={() => clickMonth('back')}
+                  className="mealList--body--month--arrowBack"/>
+                  <div>{month}월</div>
+                <IoIosArrowForward onClick={() => clickMonth('forward')}
+                  className="mealList--body--month--arrowForward"/>
               </>
             ) : (
               <></>
@@ -88,13 +73,23 @@ const Main = ({ inputingSchool, gettingSchool, closeModal, gettingMonthMeal, mea
           }
         </div>
         <div className="mealList--body--mainList">
-            <MainList
-                mealList={mealList}
-            />
+            <MainList mealList={mealList}/>
         </div>
     </div>
 </div>
   );
+};
+
+Main.propTypes = {
+  inputSchool: PropTypes.func.isRequired,
+  getSchool: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  mealList: PropTypes.array.isRequired,
+  schoolList: PropTypes.array.isRequired,
+  modalIsOpen: PropTypes.bool.isRequired,
+  clickMonth: PropTypes.func.isRequired,
+  month: PropTypes.number.isRequired,
+  isLoad: PropTypes.bool.isRequired
 };
 
 export default Main;
--- WEB/schoolmeal-web/src/container/MainTemplate.js
+++ WEB/schoolmeal-web/src/container/MainTemplate.js
@@ -1,93 +1,123 @@
-import React, { Component } from 'react';
-import { inject } from 'mobx-react';
-import Modal from 'react-modal';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { inject, observer } from 'mobx-react';
 import MainCard from '../components/Cards/MainCard/MainCard';
 import SchoolCard from '../components/Cards/SchoolCard/SchoolCard';
 import Main from '../components/Main/Main';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
-Modal.setAppElement('#root');
+const MainTemplate = ({ store }) => {
+  const { meals, schoolData, processedSchoolData, getSchoolMeal, 
+    getSchoolName, setMonth, month, isLoad } = store.meal;
 
-@inject('store')
-class MainTemplate extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            schoolName: "",
-            importing: false,
-            modalIsOpen: false,
-        };
-    }
-    
-    inputingSchool = (e) => {
-        let name = e.target.value;
-        this.setState({
-            schoolName: name
-        })
-    }
+  const [schoolName, setSchoolName] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [doubleEv, setDoubleEv] = useState(false);
 
-    gettingSchool = async() => {
-        const name = this.state.schoolName;
-        const school = this.props.store.meal;
-        await school.getSchoolName(name);
-        if(school.schoolData.length){
-            if(school.schoolData.length > 1){
-                this.setState({
-                    modalIsOpen: true
-                })
-            }
-            else if(school.schoolData.length === 1) {
-                const codeData = school.schoolData[0].code;
-                const typeData = school.schoolData[0].type;
-                await school.getSchoolMeal(codeData, typeData);
-                await school.putNowSchoolData(codeData, typeData);
-                this.setState({
-                    modalIsOpen: false
-                })
-            }
+  const inputSchool = (e) => {
+    let name = e.target.value;
+    setSchoolName(name);
+  }
+
+  const getSchool = async() => {
+    if(doubleEv === false){
+      setDoubleEv(true);
+      const name = schoolName;
+      await getSchoolName(name).then(async res => {
+        if(res.length){
+          if(res.length > 1){
+            setModalIsOpen(true);
+          }
+          else if(res.length === 1) {
+            await getSchoolMeal(res[0]);
+            setModalIsOpen(false);
+          }
         } else {
-            Swal.fire('검색한 학교가 없어요!');
+          Swal.fire('검색한 학교가 없어요!');
         }
+        setDoubleEv(false);
+      }).catch(err => {
+        Swal.fire('검색중에 에러가 발생했어요!');
+        setDoubleEv(false);
+      });
+    }
+  }
+
+  const sendSchoolCode = async (schoolData) => {
+    await getSchoolMeal(schoolData);
+    closeModal();
+  }
+
+  const clickMonth = async (type) => {
+    if(month === 1 && type === 'back') return;
+
+    if(month === 12 && type === 'forward') return;
+    
+    if(type === 'back') setMonth(false);
+
+    if(type === 'forward') setMonth(true);
+
+    await getSchoolMeal(processedSchoolData);
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
+
+  const setMealDivided = (meals) => {
+    const { breakfast, lunch, dinner, date } = meals;
+
+    const newBreakfast = mapMeals(breakfast);
+    const newLunch = mapMeals(lunch);
+    const newDinner = mapMeals(dinner);
+
+    const newMeals = {
+      breakfast: newBreakfast,
+      lunch: newLunch,
+      dinner: newDinner,
+      date: date
     }
 
-    gettingMonthMeal = async() => {
-        const meal = this.props.store.meal;
-        const code = meal.nowSchoolData.code;
-        const type = meal.nowSchoolData.type;
+    return newMeals;
+  }
 
-        await meal.getSchoolMeal(code, type);
-        this.setState({
-            modalIsOpen: false
-        })
-    }
-     
-    closeModal = () => {
-    this.setState({
-        modalIsOpen: false
-    });
+  const mapMeals = (meals) => {
+    let mappedMeals = [];
+
+    for(let meal of meals){
+      const newMeal = meal.concat(' ');
+      mappedMeals.push(newMeal);
     }
 
-    render() {
-        const { store } = this.props;
-        const mealList = store.meal.meals.map((data, i) => {
-            return <MainCard mealData={data} key={i}/>
-        });
-        const schoolList = store.meal.schoolData.map((data, i) => {
-            return <SchoolCard schoolData={data} school={this.props.store.meal} closeModal={this.closeModal} key={i}/>
-        })
-        return (
-           <Main
-            inputingSchool={this.inputingSchool}
-            gettingSchool={this.gettingSchool}
-            closeModal={this.closeModal}
-            gettingMonthMeal={this.gettingMonthMeal}
-            mealList={mealList}
-            schoolList={schoolList}
-            store={store}
-            modalIsOpen={this.state.modalIsOpen}
-           />
-        );
-    }
-}
+    return mappedMeals;
+  }
 
-export default MainTemplate;
+  const mealList = meals.map((data, i) => {
+    const mealData = setMealDivided(data);
+    return <MainCard mealData={mealData} key={i}/>
+  });
+
+  const schoolList = schoolData.map((data, i) => {
+    return <SchoolCard schoolData={data} sendSchoolCode={sendSchoolCode} key={i}/>
+  });
+
+  return (
+    <Main
+      inputSchool={inputSchool}
+      getSchool={getSchool}
+      closeModal={closeModal}
+      mealList={mealList}
+      schoolList={schoolList}
+      modalIsOpen={modalIsOpen}
+      clickMonth={clickMonth}
+      month={month}
+      isLoad={isLoad}
+    />
+  );
+};
+
+MainTemplate.propTypes = {
+    store: PropTypes.object.isRequired
+};
+
+export default inject('store')(observer(MainTemplate));
